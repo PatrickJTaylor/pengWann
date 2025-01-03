e758cc6dd45ac331b5f9c710b4de63d43a2e61b4
Patrick J. Taylor <patrick.joseph.taylor@gmail.com>
2025-01-03 10:56:57 +0000

    Adapt tests to new logic

--- tests/test_dos.py
+++ tests/test_dos.py
@@ -145,8 +145,12 @@ def test_DOS_project(load_dos, datadir) -> None:
 
 
 def test_DOS_get_descriptors(load_dos, datadir) -> None:
-    wannier_interaction_1 = WannierInteraction(i=1, j=0, R_2=np.array([-1, 1, 0]))
-    wannier_interaction_2 = WannierInteraction(i=1, j=7, R_2=np.array([-1, 0, 0]))
+    wannier_interaction_1 = WannierInteraction(
+        i=1, j=0, R_1=np.array([0, 1, 0]), R_2=np.array([0, 0, 0])
+    )
+    wannier_interaction_2 = WannierInteraction(
+        i=5, j=6, R_1=np.array([0, 1, 1]), R_2=np.array([0, 0, 0])
+    )
     interactions = (
         AtomicInteraction(
             pair_id=("C1", "C2"),
