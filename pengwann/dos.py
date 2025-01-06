df741e36323d7240a57f9d6b1e190303f68b52b2
Patrick J. Taylor <patrick.joseph.taylor@gmail.com>
2025-01-06 16:04:46 +0000

    v.minor black reformatting

--- pengwann/dos.py
+++ pengwann/dos.py
@@ -348,7 +348,9 @@ class DOS:
 
         descriptors = self.get_descriptors(all_interactions, calculate_wobi=False)
 
-        return np.sum([descriptor["WOHP"] for descriptor in descriptors.values()], axis=0)
+        return np.sum(
+            [descriptor["WOHP"] for descriptor in descriptors.values()], axis=0
+        )
 
     def get_descriptors(
         self,
