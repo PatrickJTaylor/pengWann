97c3d941a83be7db3da38a92cf341a27ed04447b
Patrick J. Taylor <patrick.joseph.taylor@gmail.com>
2025-01-06 15:54:56 +0000

    Update tests

--- tests/test_dos.py
+++ tests/test_dos.py
@@ -269,6 +269,27 @@ def test_DOS_get_descriptors_k_resolved(load_dos, datadir) -> None:
     np.testing.assert_allclose(test_WOBI, ref_WOBI)
 
 
+def test_DOS_get_density_of_energy(load_dos, datadir) -> None:
+    wannier_interaction_1 = WannierInteraction(
+        i=1, j=0, R_1=np.array([0, 1, 0]), R_2=np.array([0, 0, 0])
+    )
+    wannier_interaction_2 = WannierInteraction(
+        i=5, j=6, R_1=np.array([0, 1, 1]), R_2=np.array([0, 0, 0])
+    )
+    interactions = (
+        AtomicInteraction(
+            pair_id=("C1", "C2"),
+            wannier_interactions=(wannier_interaction_1, wannier_interaction_2),
+        ),
+    )
+
+    num_wann = 7
+    test_doe = load_dos.get_density_of_energy(interactions, num_wann)
+    ref_doe = np.load(f"{datadir}/doe.npy")
+
+    np.testing.assert_allclose(test_doe, ref_doe)
+
+
 def test_DOS_integrate_descriptors(load_dos, datadir) -> None:
     wohp = np.load(f"{datadir}/total_WOHP.npy")
     wobi = np.load(f"{datadir}/total_WOBI.npy")
