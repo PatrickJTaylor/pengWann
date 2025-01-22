b6ce42d4b637948388e1f004128f0e613cdeb8a7
Patrick J. Taylor <patrick.joseph.taylor@gmail.com>
2025-01-22 11:13:22 +0000

    Add copyright notices to source files

--- pengwann/geometry.py
+++ pengwann/geometry.py
@@ -6,6 +6,21 @@ and from this identify relevant interatomic interactions from which to compute b
 descriptors.
 """
 
+# Copyright (C) 2024-2025 Patrick J. Taylor
+
+# This file is part of pengWann.
+#
+# pengWann is free software: you can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# pengWann is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with pengWann.
+# If not, see <https://www.gnu.org/licenses/>.
+
 from __future__ import annotations
 
 import numpy as np
--- pengwann/io.py
+++ pengwann/io.py
@@ -7,6 +7,21 @@ parsing all the data required to construct an instance of the
 :py:class:`~pengwann.descriptors.DescriptorCalculator` class.
 """
 
+# Copyright (C) 2024-2025 Patrick J. Taylor
+
+# This file is part of pengWann.
+#
+# pengWann is free software: you can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# pengWann is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with pengWann.
+# If not, see <https://www.gnu.org/licenses/>.
+
 from __future__ import annotations
 
 import os
