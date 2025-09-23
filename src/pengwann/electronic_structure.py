5c21e64654532c3ddcf050c157c2c2f2e447665d
Patrick J. Taylor <patrick.joseph.taylor@gmail.com>
2025-09-23 15:08:52 +0100

    Add function to evaluate the total DOS

--- src/pengwann/electronic_structure.py
+++ src/pengwann/electronic_structure.py
@@ -24,3 +24,19 @@ from numpy.typing import NDArray
 class Basis(NamedTuple):
     u: NDArray[np.complex128]
     kpoints: NDArray[np.float64]
+
+
+def compute_total_density_of_states(
+    eigenvalues: NDArray[np.float64],
+    energy_range: tuple[float, float],
+    resolution: float,
+    sigma: float,
+) -> NDArray[np.float64]:
+    emin, emax = energy_range
+    energies = np.arange(emin, emax + resolution, resolution, dtype=np.float64)
+
+    x_mu = energies[:, np.newaxis, np.newaxis] - eigenvalues
+    dos = 1 / np.sqrt(np.pi * sigma) * np.exp(-(x_mu**2) / sigma) / eigenvalues.shape[1]
+    dos = np.swapaxes(dos, 1, 2)
+
+    return dos
