5d014033bb306980cfdb03090c5b01b756724060
Patrick J. Taylor <patrick.joseph.taylor@gmail.com>
2025-09-25 17:42:05 +0100

    Start refactor of interactions module and add finders to geometry

--- src/pengwann/geometry.py
+++ src/pengwann/geometry.py
@@ -28,6 +28,12 @@ from typing import NamedTuple, TYPE_CHECKING
 import numpy as np
 from numpy.typing import NDArray
 
+from pengwann.interactions import (
+    AtomicInteraction,
+    AtomicInteractions,
+    WannierInteraction,
+)
+
 if TYPE_CHECKING:
     from ase import Atoms
     from pymatgen.core import Structure
@@ -68,6 +74,78 @@ class Geometry(NamedTuple):
         to_print += assignment_lines + ["", "Sites", "-----"] + site_lines
 
         return "\n".join(to_print) + "\n"
+
+    def find_onsite_interactions(self, symbols: tuple[str, ...]) -> AtomicInteractions:
+        zero_vector = np.array([0, 0, 0])
+
+        interactions = []
+        for idx, symbol in enumerate(self.symbols):
+            if symbol in symbols:
+                wannier_interactions = []
+                for i in self.wannier_assignments[idx]:
+                    wannier_interaction = WannierInteraction(
+                        i, i, zero_vector, zero_vector
+                    )
+
+                    wannier_interactions.append(wannier_interaction)
+
+                interaction = AtomicInteraction(
+                    idx, idx, symbol, symbol, tuple(wannier_interactions)
+                )
+                interactions.append(interaction)
+
+        if not interactions:
+            raise ValueError(f"No atoms matching symbols in {symbols} found.")
+
+        return AtomicInteractions(tuple(interactions))
+
+    def find_interatomic_interactions(
+        self,
+        radial_cutoffs: dict[tuple[str, str], float],
+    ) -> AtomicInteractions:
+        symbols = tuple({symbol for pair in radial_cutoffs for symbol in pair})
+        atom_indices = self._atom_labelled_indices(symbols)
+
+        interactions = []
+        for pair, cutoff in radial_cutoffs.items():
+            symbol_i, symbol_j = pair
+
+            offset = 1 if symbol_i == symbol_j else 0
+
+            for idx, i in enumerate(atom_indices[symbol_i]):
+                for j in atom_indices[symbol_j][idx + offset :]:
+                    distance = self.distance_matrix[i, j]
+
+                    if distance < cutoff:
+                        wannier_interactions = []
+                        for m in self.wannier_assignments[i]:
+                            for n in self.wannier_assignments[j]:
+                                bl_i = self.image_matrix[i, m]
+                                bl_j = self.image_matrix[j, n]
+
+                                wannier_interaction = WannierInteraction(
+                                    m, n, bl_i, bl_j
+                                )
+                                wannier_interactions.append(wannier_interaction)
+
+                        wannier_interactions = tuple(wannier_interactions)
+                        interaction = AtomicInteraction(
+                            i, j, symbol_i, symbol_j, wannier_interactions
+                        )
+                        interactions.append(interaction)
+
+        return AtomicInteractions(tuple(interactions))
+
+    def _atom_labelled_indices(self, symbols: tuple[str, ...]) -> dict[str, list[int]]:
+        atom_indices = {}
+        for symbol in symbols:
+            atom_indices[symbol] = []
+
+        for idx, symbol in enumerate(self.symbols):
+            if symbol in symbols:
+                atom_indices[symbol].append(idx)
+
+        return atom_indices
 
     def as_atoms(self) -> Atoms:
         try:
