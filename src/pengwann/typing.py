c40740c3ae7e14c5c6c939125d901d0ad5e73acd
Patrick J. Taylor <patrick.joseph.taylor@gmail.com>
2025-10-12 15:02:47 +0100

    Explicitly declare aliases as TypeAlias

--- src/pengwann/typing.py
+++ src/pengwann/typing.py
@@ -1,4 +1,5 @@
 from collections.abc import Callable, Sequence
+from typing import TypeAlias
 
 import numpy as np
 from numpy.typing import NDArray
@@ -9,6 +10,6 @@ from pengwann.interactions import (
     WannierInteraction,
 )
 
-Hamiltonian = dict[tuple[int, int, int], NDArray[np.complex128]]
-Interactions = AtomicInteractions | AtomicInteraction | Sequence[WannierInteraction]
-WannierTransform = Callable[[WannierInteraction], WannierInteraction]
+Hamiltonian: TypeAlias = dict[tuple[int, int, int], NDArray[np.complex128]]
+Interactions: TypeAlias = AtomicInteractions | AtomicInteraction | Sequence[WannierInteraction]
+WannierTransform: TypeAlias = Callable[[WannierInteraction], WannierInteraction]
